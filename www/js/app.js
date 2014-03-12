20f00af5259edce9ebc31564b41d48750ef72b40
Tyler Fisher <tyler@tylerjfisher.com>
2014-03-12 11:19:33 -0400

    Support for vertical chapters and horizontal slides

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,17 @@
 var $w;
 var $h;
 var $slides;
+var $currentChapter;
+var $currentSlideshow;
+var index = 0;
+var currentSlide;
+
+var setSlideHeight = function() {
+    $w = $(window).width();
+    $h = $(window).height();
+    $slides.css('height', $h);
+    $slides.css('width', $w);
+};
 
 var setUpPanelSnap = function() {
     var options = {
@@ -10,6 +21,14 @@ var setUpPanelSnap = function() {
         panelSelector: 'section',
         onSnapStart: function(){
         },
+        onSnapFinish: function(){
+            index = 0;
+            $currentChapter = $('.chapter.active');
+            $currentSlideshow = $currentChapter.find('.slide');
+            currentSlide = $currentSlideshow[index];
+
+            console.log(currentSlide);
+        },
         onActivate: function() {
         }
     };
@@ -17,17 +36,35 @@ var setUpPanelSnap = function() {
     $('#content').panelSnap(options);
 };
 
-var setSlideHeight = function() {
-    $w = $(window).width();
-    $h = $(window).height();
-    $slides.css('height', $h);
-    $slides.css('width', $w);
+var handleKeyPress = function(e) {
+    if (e.keyCode === 37 && index >= 0) {
+        index--;
+        $(currentSlide).removeClass('present');
+        currentSlide = $currentSlideshow[index];
+        $(currentSlide).addClass('present');
+
+        console.log(currentSlide);
+    }
+
+    if (e.keyCode === 39 && index < $currentSlideshow.length - 1) {
+        index++;
+        $(currentSlide).removeClass('present');
+        currentSlide = $currentSlideshow[index];
+        $(currentSlide).addClass('present');
+    }
 };
 
 $(document).ready(function() {
     $slides = $('.slide');
+    $currentChapter = $('.chapter.active');
+    $currentSlideshow = $currentChapter.find('.slide');
+    currentSlide = $currentSlideshow[index];
+
     setSlideHeight();
     setUpPanelSnap();
 
+    console.log($currentSlideshow);
+
+    $(document).keydown(handleKeyPress);
     $(window).resize(setSlideHeight);
 });
