9a7286d2d2e2fb0b9adccaf80f1ffd044717d540
lindamood <wesley.lindamood@gmail.com>
2014-03-24 16:31:23 -0400

    making room for nav on video section

--- www/js/app.js
+++ www/js/app.js
@@ -318,8 +318,8 @@ var initPlayer = function(player) {
         controlbar: 'over',
         icons: 'true',
         autostart: false,
-        width: $w,
-        height: $h
+        width: '100%',
+        height: $h - 50
     });
     jwplayer('player').play();
 
