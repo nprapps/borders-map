8724235c0fd96527c553b641090d9972701d8cde
lindamood <wesley.lindamood@gmail.com>
2014-03-25 14:20:13 -0400

    primary nav styling

--- www/js/app.js
+++ www/js/app.js
@@ -276,7 +276,7 @@ var onTitlecardButtonClick = function() {
 };
 
 var showNav = function() {
-    $nav.height($h);
+    //$nav.height($h);
     $navButton.find('i').toggleClass('fa-bars').toggleClass('fa-times');
     $nav.toggleClass('active');
     if ($nav.hasClass('active')) {
